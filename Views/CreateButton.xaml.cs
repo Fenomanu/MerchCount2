383c6770e33ca7fdc12e5dfff6fe99a042ba1966
Fenomanu <61010890+Fenomanu@users.noreply.github.com>
2023-12-20 11:53:00 +0100

    Changes

--- Views/CreateButton.xaml.cs
+++ Views/CreateButton.xaml.cs
@@ -4,13 +4,19 @@ namespace MerchCount2.Views;
 
 public partial class CreateButton : ContentView
 {
-    public Button Button;
+    public CreateButton Button;
+    public event EventHandler Clicked;
 
 
     public CreateButton()
 	{
+        Button = this;
 		InitializeComponent();
-        Button = ButtonText ?? new Button();
-        Button.ImageSource = "Resources/Icons/addpng.png";
+        var tapGesture = new TapGestureRecognizer();
+        tapGesture.Tapped += (s, e) => {
+            // Invoca el evento Clicked
+            Clicked?.Invoke(this, EventArgs.Empty);
+        };
+        this.GestureRecognizers.Add(tapGesture);
     }
 }
